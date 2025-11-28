cb1aa91f9a4a400a281560d8af7599bb7fe5ae18
Lesly Cedillo <125913655+Lesly2108@users.noreply.github.com>
2025-11-28 14:38:54 -0600

    Update ticket.js

--- js/ticket.js
+++ js/ticket.js
@@ -1,1 +1,22 @@
+let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+
+const div = document.getElementById("ticket");
+
+if (carrito.length === 0) {
+    div.innerHTML = "<p>No hay compras en el carrito.</p>";
+} else {
+    let total = 0;
+
+    carrito.forEach(p => {
+        total += p.precio;
+        div.innerHTML += `<p>${p.nombre} - $${p.precio}</p>`;
+    });
+
+    div.innerHTML += `<hr><p><strong>Total: $${total.toFixed(2)}</strong></p>`;
+}
+
+function limpiar() {
+    localStorage.removeItem("carrito");
+    window.location.href = "index.html";
+}
 
